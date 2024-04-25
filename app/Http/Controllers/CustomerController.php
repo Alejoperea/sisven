8ad382f9a48d17d85aa88a4ecd94e454a27e7b38
Richard Alvarez <richardandrewalvarez1207@gmail.com>
2024-04-24 20:58:38 -0500

    Creacion de Index para Customers

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\Customer;
 
 class CustomerController extends Controller
 {
@@ -11,7 +13,8 @@ class CustomerController extends Controller
      */
     public function index()
     {
-        //
+        $customers = Customer::all();
+        return view('customer.index', ['customers' => $customers]);
     }
 
     /**
